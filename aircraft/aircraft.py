82685c306414b6bac137509e8bf5a2c10be7a727
Carlos Linares <contact@carloslinares.eu>
2020-11-09 21:35:27 +0100

    Adding Mission class and Excel file

--- aircraft/aircraft.py
+++ aircraft/aircraft.py
@@ -1,10 +1,10 @@
 class AircraftType():
 
-    def __init__(self, name, role, empty_weight, max_to_weight, clean_lift2drag, dirty_lift2drag, tsfc):
+    def __init__(self, name, role, empty_weight, max_takeoff_weight, clean_lift2drag, dirty_lift2drag, tsfc):
         self.name = name
         self.role = role
         self.empty_weight = empty_weight
-        self.max_to_weight = max_to_weight
+        self.max_takeoff_weight = max_takeoff_weight
         self.clean_lift2drag = clean_lift2drag
         self.dirty_lift2drag = dirty_lift2drag
         self.tsfc = tsfc
--- mission/mission.py
+++ mission/mission.py
@@ -1,25 +1,25 @@
-import math
+import os, math, openpyxl
+
+def scan_mission_file():
+    return 0
+
 
 class Mission():
-    def __init__ (self,id,name,mission_type,mission_range,payload):
-        self.id = id
+    def __init__ (self,id_,name,mission_type,mission_range,payload):
+        self.id_ = id_
         self.name = name
         self.mission_type = mission_type
         self.range = mission_range
         self.payload = payload
         self.segments = []
-    
-    #def add_mission_segment(self,mission_segment):
-        
-        #self.segments.append
 
-    #def calculate_cruising_range(self):
-
+    def add_mission_segment(self,mission_segment):
+        self.segments.append(mission_segment)
 
 
 class MissionSegment():
-    def __init__ (self, id, name, aero_setup):
-        self.id = id
+    def __init__ (self, id_, name, aero_setup):
+        self.id_ = id_
         self.name = name
         self.aero_setup = aero_setup
 
@@ -31,15 +31,15 @@ class MissionSegment():
             self.lift2drag = aircraft_type.clean_lift2drag
         else:
             self.lift2drag = aircraft_type.clean_lift2drag
-            print("Unknown aero_setup. Using clean_lift2drag for segment "+ self.id +"_" + self.name)
+            print("Unknown aero_setup. Using clean_lift2drag for segment "+ self.id_ +"_" + self.name)
     
     def payload_drop(self, payload):
         self.weight_delta = -payload
 
 class Cruise(MissionSegment):
     
-    def __init__(self, id, name, aero_setup, speed, lenght):
-        MissionSegment.__init__(self, id, name, aero_setup)
+    def __init__(self, id_, name, aero_setup, speed, lenght):
+        MissionSegment.__init__(self, id_, name, aero_setup)
         self.lenght = lenght
         self.speed = speed
 
@@ -49,8 +49,8 @@ class Cruise(MissionSegment):
 
 class Gradient(MissionSegment):
 
-    def __init__(self, id, name, aero_setup, speed, climb_gradient, initial_altitude, final_altitude):
-        MissionSegment.__init__(self, id, name, aero_setup)
+    def __init__(self, id_, name, aero_setup, speed, climb_gradient, initial_altitude, final_altitude):
+        MissionSegment.__init__(self, id_, name, aero_setup)
         self.climb_gradient = climb_gradient
         self.initial_altitude = initial_altitude
         self.final_altitude = final_altitude
@@ -64,8 +64,8 @@ class Gradient(MissionSegment):
 
 class Loitering(MissionSegment):
 
-    def __init__(self, id, name, aero_setup, endurance):
-        MissionSegment.__init__(self, id, name, aero_setup)
+    def __init__(self, id_, name, aero_setup, endurance):
+        MissionSegment.__init__(self, id_, name, aero_setup)
         self.endurance = endurance
 
     def breguet(self):
@@ -74,15 +74,15 @@ class Loitering(MissionSegment):
 
 class Estimated(MissionSegment):
 
-    def __init__(self, id, name, aero_setup, estimated_weight_fraction):
-        MissionSegment.__init__(self, id, name, aero_setup)
+    def __init__(self, id_, name, aero_setup, estimated_weight_fraction):
+        MissionSegment.__init__(self, id_, name, aero_setup)
         self.estimated_weight_fraction = estimated_weight_fraction
 
 
 class Combat(MissionSegment):
 
-    def __init__(self, id, name, aero_setup, combat_time):
-        MissionSegment.__init__(self, id, name, aero_setup)
+    def __init__(self, id_, name, aero_setup, combat_time):
+        MissionSegment.__init__(self, id_, name, aero_setup)
         self.combat_time  = combat_time
     
     def breguet(self):
